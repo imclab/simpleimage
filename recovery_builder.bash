e62fbfde69e03dd46fa58f664afe59187d0120b3
will-i-am-79 <chris.gerke@gmail.com>
2014-07-08 09:08:18 +1000

    RecoveryHD
    The makings of a bootable USB flash system for DMG deployment.

--- recovery_builder.bash
+++ recovery_builder.bash
@@ -12,9 +12,8 @@ fi
 #++ setup some variables
 osx_version=$(defaults read "/System/Library/CoreServices/SystemVersion" ProductVersion)	# 10.9.2 etc
 osx_build=$(defaults read "/System/Library/CoreServices/SystemVersion" ProductBuildVersion) # 13A603 etc
-readwrite_dmg=~/Desktop/${osx_version}_${osx_build}_recovery_RW.dmg
-target_dmg=~/Desktop/${osx_version}_${osx_build}_recovery.dmg
 basesystem_dmg="/Volumes/OS X Install ESD/BaseSystem.dmg" # APPLE'S BASE SYSTEM, THE BOOTABLE RECOVERY OS
+target_dmg=~/Desktop/${osx_version}_${osx_build}_recovery.dmg
 
 # must sudo
 if [[ $(id -u) -ne 0 ]]; then
@@ -27,9 +26,9 @@ if [ $# -eq 0 ]; then
 cat <<EOF
 
 	Usage:
-	sudo $(basename "$0") [--dmg --pkg --iso]
-		--dmg 	Create DMG image.
-		--pkg 	Create createOSXinstallPkg package.
+	sudo $(basename "$0") PATHTOINSTALLESD [--iso]
+		--dmg 	Create DMG image. coming soon.
+		--pkg 	Create createOSXinstallPkg package. coming soon.
 		--iso 	Create Virtualbox bootable image.
 
 	Description:
@@ -44,81 +43,84 @@ EOF
 exit 1
 fi
 
-#++ house keeping
-declare -a disk_mounts
-eject_disks() {
-    unset tempdirs
-    for mount_point in "${disk_mounts[@]}"; do
-    if [[ -d "${mount_point}" ]]; then
-        if ! hdiutil eject "${mount_point}"; then
-            for tries in {1..10}; do
-                sleep ${tries}
-                if hdiutil eject "${mount_point}" -force 2>/dev/null; then
-                    break
-                fi
-            done
-        fi
-    fi
-    done
-    unset disk_mounts
-    return 0
-}
+if [ $# -eq 0 ]; then
+	usage
+	exit 1
+fi
 
 #++ mount installesd source file
 if [ -e "${1}" ]; then
-	attached_installesd=$(hdiutil attach "${1}" -nobrowse -owners on)
-	disk_mounts+=("${attached_installesd}")
+	hdiutil attach "${1}" -nobrowse -owners on
 	if [ $? -ne 0 ]; then
 		echo "error attaching ${1} ... exiting"
 		exit 1
 	fi
 fi
+sleep 5
 
 #++ convert basesystem.dmg to RW
 if [ -e "${basesystem_dmg}" ]; then
-	sudo hdiutil convert -format UDRW -o ${readwrite_dmg} "${basesystem_dmg}"
+	basesystem_temp="$(mktemp -d ~/Desktop/BaseSystemRW.XXXX)"
+	sudo hdiutil convert -format UDRW -o ${basesystem_temp} "${basesystem_dmg}"
 	if [ $? -ne 0 ]; then
 		echo "error converting ${basesystem_dmg}"
-		XIT 1
+		exit 1
 	fi
-else
-	echo "error finding ${basesystem_dmg}"
+fi
+sleep 5
+
+#++ eject disks, if they aren't the next conversion won't work
+hdiutil detach "/Volumes/OS X Install ESD" -force
+if [ $? -ne 0 ]; then
+	echo "error detaching /Volumes/OS X Install ESD"
 	exit 1
 fi
+sleep 5
 
 #++ attach RW basesytem.dmg
-sleep 5
-attached_basedmg=$(hdiutil attach "${readwrite_dmg}" -nobrowse -owners on)
-disk_mounts+=("${attached_basedmg}")
+hdiutil attach "${basesystem_temp}.dmg" -mountpoint "${basesystem_temp}" -nobrowse -owners on
 if [ $? -ne 0 ]; then
-	echo "error attaching ${readwrite_dmg}"
-	XIT 1
+	echo "error attaching ${attached_basedmg}"
+	exit 1
 fi
+sleep 5
 
 # rename temp BaseSystem.dmg VOLUME
 # probably need something smarter here to detect /dev/disks etc
-if [ -d "/Volumes/OS X Base System" ]; then
-	sudo diskutil rename "OS X Base System" "Custom OS X Base System"
-	if [ $? -ne 0 ]; then
-		echo "error renaming '/Volumes/OS X Base System' to 'Custom OS X Base System'"
-	fi
+sudo diskutil rename "OS X Base System" "Custom OS X Base System"
+if [ $? -ne 0 ]; then
+	echo "error renaming '/Volumes/OS X Base System' to 'Custom OS X Base System'"
 fi
+
+#++ setup the DMG storage
+#++ this folder will contain the DMG or PKG files used in the Recovery HD menu/automation script
+mkdir "${basesystem_temp}/INSTALL"
 
 #++ eject disks, if they aren't the next conversion won't work
-eject_disks
+hdiutil detach "${basesystem_temp}" -force
+if [ $? -ne 0 ]; then
+	echo "error detaching Custom OS X Base System"
+	exit 1
+fi
+sleep 5
 
-sudo hdiutil convert -format UDZO -o ${target_dmg} "${readwrite_dmg}"
+sudo hdiutil convert -format UDZO -o ${target_dmg} "${basesystem_temp}.dmg"
 if [ $? -ne 0 ]; then
 	echo "error UDZO ... exiting."
-	XIT 1
+	exit 1
 fi
+sleep 5
+
+#++ clean up
+rm -R "${basesystem_temp}"
+rm "${basesystem_temp}.dmg"
 
 #++ asr scan
-sleep 5
 sudo asr imagescan --source ${target_dmg}
 if [ $? -ne 0 ]; then
 	echo "asr scan for restore failed."
 fi
+sleep 5
 
 if [[ ${ISO} = 1 ]]; then
    hdiutil convert "${target_dmg}" -format UDTO -o "${target_dmg}.iso"
